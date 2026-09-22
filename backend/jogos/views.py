16042c6c0386d5cec87496797b933803fd4becb5
JMGOUVEA7 <jm.carvalho@gmail.com>
2026-09-22 16:47:49 -0300

    adicionado API REST (no views) e Serializer

--- backend/jogos/views.py
+++ backend/jogos/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Jogo
+from .serializer import JogoSerializer
 
-# Create your views here.
+
+# Listar jogos
+
+class ListaJogosView(generics.ListCreateAPIView):
+    serializer_class = JogoSerializer
+
+    def get_queryset(self):
+        queryset = Jogo.objects.all()
+
+        nome = self.request.query_params.get('nome')
+
+        if nome:
+            queryset = queryset.filter(nome__icontains=nome) #faz com que a pesquisa não diferencie letras maiúsculas de minúsculas
+
+        return queryset
+
+
+
+class DetalheJogoView(generics.RetrieveDestroyAPIView):
+    queryset = Jogo.objects.all()
+    serializer_class = JogoSerializer
+
+
