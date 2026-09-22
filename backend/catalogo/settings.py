592a0da92cefb5e24962d1477bb77012f022d8a0
JMGOUVEA7 <jm.carvalho@gmail.com>
2026-09-22 15:40:45 -0300

    adicionado models e tabelas criadas no banco

--- backend/catalogo/settings.py
+++ backend/catalogo/settings.py
@@ -130,3 +130,5 @@ MAILERS = {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
 }
+
+CORS_ALLOW_ALL_ORIGINS = True
--- backend/jogos/models.py
+++ backend/jogos/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Jogo(models.Model):
+    nome = models.CharField(max_length=100)
+    genero = models.CharField(max_length=50)
+    plataforma = models.CharField(max_length=50)
+    ano_lancamento = models.IntegerField()
+    favorito = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nome
+
